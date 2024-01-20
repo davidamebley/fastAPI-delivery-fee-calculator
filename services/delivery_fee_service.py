594978ca0a384645d2d4d1cc4f79ac46a62287a0
David Amebley <davameb@gmail.com>
2024-01-21 00:01:00 +0200

    Create a new function that will handle the delivery fee calculation logic in the delivery_fee_service.py file

---
+++ services/delivery_fee_service.py
@@ -0,0 +1,10 @@
+from models.delivery_fee_models import DeliveryFeeRequest
+
+
+def calculate_fee(request: DeliveryFeeRequest) -> int:
+    """
+    Calculates the delivery fee for a given request.
+    """
+    calculated_fee = 0
+
+    return calculated_fee
