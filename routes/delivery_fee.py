9596de5125ed26715b89ddf78def506f387f55db
David Amebley <davameb@gmail.com>
2024-01-22 02:35:49 +0200

    Implement custom validations to raise HTTPExceptions when negative values are entered

--- routes/delivery_fee.py
+++ routes/delivery_fee.py
@@ -1,12 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from models.delivery_fee_models import DeliveryFeeRequest, DeliveryFeeResponse
-from services.delivery_fee_service import calculate_fee
+from services.delivery_fee_service import calculate_delivery_fee
 
 
 router = APIRouter()
 
 @router.post("/calculate-delivery-fee", response_model=DeliveryFeeResponse)
-async def calculate_delivery_fee(request: DeliveryFeeRequest) -> DeliveryFeeResponse:
-    fee = calculate_fee(request)
+async def calculate_delivery_fee_api(request: DeliveryFeeRequest) -> DeliveryFeeResponse:
+    # Custom Validations
+    if request.cart_value < 0:
+        raise HTTPException(
+            status_code=400, detail="Cart value cannot be negative."
+        )
+    if request.delivery_distance < 0:
+        raise HTTPException(
+            status_code=400, detail="Delivery distance cannot be negative."
+        )
+    if request.number_of_items < 0:
+        raise HTTPException(
+            status_code=400, detail="Number of items cannot be negative."
+        )
+    
+
+    fee = calculate_delivery_fee(request)
     return DeliveryFeeResponse(delivery_fee=fee)
