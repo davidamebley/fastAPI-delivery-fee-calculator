dbf9d35f9e3f076efced8cf69c1d33340a5fcb34
David Amebley <davameb@gmail.com>
2024-01-23 04:40:37 +0200

    Add new constants for delivery fee calculation to replace static values in code logic

--- services/delivery_fee_service.py
+++ services/delivery_fee_service.py
@@ -1,5 +1,23 @@
 from models.delivery_fee_models import DeliveryFeeRequest
 
+# Constants
+BASE_CART_VALUE_CENTS = 1000 # Minimum cart value in cents to avoid small order surcharge.
+BASE_DISTANCE_METERS = 1000 # Base distance in meters for initial delivery fee calculation.
+ADDITIONAL_DISTANCE_INTERVAL_METERS = 500   #   Interval in meters for calculating additional distance fee.
+ADDITIONAL_DISTANCE_ROUNDING = ADDITIONAL_DISTANCE_INTERVAL_METERS - 1  # Value used to ensure rounding up for additional distance fee calculation.
+BASE_DELIVERY_FEE_CENTS = 200   # Base delivery fee in cents.
+ADDITIONAL_DISTANCE_FEE_CENTS = 100 # Additional fee in cents for each extra distance interval.
+ITEMS_THRESHOLD_FOR_EXTRA_FEE = 5   # Number of items threshold from which extra fee per item is applied.
+BULK_THRESHOLD = 12 # Number of items threshold beyond which bulk fee is applied.
+ADDITIONAL_ITEM_COUNT_FEE_CENTS = 50    # Additional fee in cents for each item beyond the ITEMS_THRESHOLD_FOR_EXTRA_FEE.
+BULK_FEE_CENTS = 120    # Additional bulk fee in cents when itemcount exce BULK_THRESHOLD.
+MAX_FEE_CENTS = 1500    # Maximum delivery fee cap in cents.
+RUSH_HOUR_MULTIPLIER = 1.2  # Multiplier for delivery fee during rush hours.
+RUSH_HOUR_DAY = 4   # (0=Monday, 1=Tuesday, ..., 6=Sunday) for rush hour calculation.
+RUSH_HOUR_START = 15    # Start hour (24-hour format) for rush hour fee calculation.
+RUSH_HOUR_END = 19  # End hour (24-hour format) for rush hour fee calculation.
+FREE_DELIVERY_CART_VALUE_CENTS = 20000  # Cart value in cents from which delivery is free.
+
 
 def calculate_delivery_fee(request: DeliveryFeeRequest) -> int:
     """
@@ -20,7 +38,7 @@ def calculate_delivery_fee(request: DeliveryFeeRequest) -> int:
         fee += ((additional_distance + 499) // 500) * 100
     
     # Item Count Surcharge
-    if request.number_of_items >= 5:
+    if request.number_of_items > 4:
         extra_items = request.number_of_items - 4
         fee += extra_items * 50 # 50 cents per extra item
         if request.number_of_items > 12:
@@ -35,7 +53,7 @@ def calculate_delivery_fee(request: DeliveryFeeRequest) -> int:
 
     # Rush Hour Fee
     order_time = request.time
-    if order_time.weekday() == 4 and 15 <= order_time.hour <= 19:    # Fridays, 3pm to 7pm
-        fee = int(min(fee * 1.2, 1500))    # total fee times 1.2, but limit to 15€ max
+    if order_time.weekday() == 4 and 15 <= order_time.hour <= 19:    # Rush hour period
+        fee = int(min(fee * 1.2, 1500))    # Total fee x multiplier, but limit to max fee
     
     return fee
