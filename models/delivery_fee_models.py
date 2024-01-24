126fbc4a0747734fd87d334d15ec04cae4057ec9
David Amebley <davameb@gmail.com>
2024-01-24 04:56:04 +0200

    Add comments in the delivery_fee_models.py to describe portions of the code

--- models/delivery_fee_models.py
+++ models/delivery_fee_models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from pydantic import BaseModel
 
-
+# Model for incoming delivery fee calculation request.
 class DeliveryFeeRequest(BaseModel):
     cart_value: int
     delivery_distance: int
     number_of_items: int
     time: datetime
 
+# Model for outgoing delivery fee calculation response.
 class DeliveryFeeResponse(BaseModel):
     delivery_fee: int
