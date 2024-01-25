9e4f013f551774a1d52cdacb98025b28e6fc13e2
David Amebley <davameb@gmail.com>
2024-01-25 05:54:36 +0200

    Write a unit test to assert apply_small_order_surcharge() with a cart value below the base cart value.

--- tests/test_fee_calculations.py
+++ tests/test_fee_calculations.py
@@ -2,3 +2,11 @@ from services.fee_calculations import *
 
 from constants import *
 
+def test_apply_small_order_surcharge_below_threshold():
+    """
+    Test apply_small_order_surcharge() with a cart value below the base cart value.
+    """
+    cart_value = 1000
+    expected_fee = 0
+    fee = apply_small_order_surcharge(cart_value)
+    assert fee == expected_fee
