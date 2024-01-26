901a232e5eb1da287b9e03b4e2baecf3b2fffb12
David Amebley <davameb@gmail.com>
2024-01-26 04:52:23 +0200

    Refactor unit tests code by making comments clearer and removing unnecessary assertions

--- tests/test_fee_calculations.py
+++ tests/test_fee_calculations.py
@@ -1,4 +1,4 @@
-import dis
+import pytest
 from turtle import distance
 from services.fee_calculations import *
 
@@ -6,27 +6,27 @@ from constants import *
 
 def test_apply_small_order_surcharge_below_threshold():
     """
-    Test apply_small_order_surcharge() with a cart value below the base cart value.
+    Test asmall order surcharge with a cart value below the base cart value.
     """
     assert apply_small_order_surcharge(500) == BASE_CART_VALUE_CENTS - 500
 
 def test_apply_small_order_surcharge_above_threshold():
     """
-    Test apply_small_order_surcharge() with a cart value above the base cart value.
+    Test small order surcharge with a cart value above the base cart value.
     """
     assert apply_small_order_surcharge(1000) == 0
     assert apply_small_order_surcharge(1500) == 0
 
 def test_calculate_additional_distance_fee_within_base_distance():
     """
-    Test calculate_additional_distance_fee() with a distance within the base distance.
+    Test additional distance fee within base distance.
     """
     assert calculate_additional_distance_fee(BASE_DISTANCE_METERS) == 0
     assert calculate_additional_distance_fee(BASE_DISTANCE_METERS-1) == 0
 
 def test_calculate_additional_distance_fee_above_base_distance():
     """
-    Test calculate_additional_distance_fee() with a distance above the base distance.
+    Test additional distance fee above base distance.
     """
     # Additional distance which should incur a fee
     additional_distance = 501
